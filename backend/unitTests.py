9585cef6c7a8e274e7b49da281e7aee18bf15710
AritroDe <dear@tcd.ie>
2022-11-24 23:27:36 +0000

    unit tests written

--- backend/unitTests.py
+++ backend/unitTests.py
@@ -1,10 +1,20 @@
+from server1 import *
+import requests
 import unittest
+import pytest
 
+def dict_of_issues_not_null():
+     issues_array = issues(url)
+     assert issues_array != 0
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+def dict_of_commits_not_null():
+     commits_array = commits(url)
+     assert commits_array != 0
 
+def dict_of_pull_requests_not_null():
+     pull_requests_array = pull_requests(url)
+     assert pull_requests_array != 0
 
-if __name__ == '__main__':
-    unittest.main()
+def dict_of_collab_issues_not_null():
+     collab_issues_array = collaberatedIssues(url)
+     assert collab_issues_array != 0
