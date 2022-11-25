fddb74bb0086a09cb3ef8c27c504a6c6f98e3913
AritroDe <dear@tcd.ie>
2022-11-25 11:58:11 +0000

    Update unitTests.py

--- backend/unitTests.py
+++ backend/unitTests.py
@@ -3,19 +3,26 @@ import requests
 import unittest
 import pytest
 
-def dict_of_issues_not_null():
-     issues_array = issues(url)
-     assert issues_array != 0
 
-def dict_of_commits_not_null():
-     commits_array = commits(url)
-     assert commits_array != 0
+def test_dict_of_issues_not_null():
+    issues_list = dict_of_issues
+    assert issues_list != []
 
-def dict_of_pull_requests_not_null():
-     pull_requests_array = pull_requests(url)
-     assert pull_requests_array != 0
 
-def dict_of_collab_issues_not_null():
-     collab_issues_array = collaberatedIssues(url)
-     assert collab_issues_array != 0
-     
+def test_dict_of_commits_not_null():
+    commits_list = dict_of_commits
+    assert commits_list != []
+
+
+def test_dict_of_pull_requests_not_null():
+    pull_requests_list = dict_of_pull_requests
+    assert pull_requests_list != []
+
+
+def test_dict_of_collab_issues_not_null():
+    collab_issues_list = dict_of_collab_issues
+    assert collab_issues_list != []
+
+if __name__ == "__main__":
+    pytest.main(['./'])
+
